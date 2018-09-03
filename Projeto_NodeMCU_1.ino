2fcb4b169acf8a35ce58b9f95b2056ed00ab1d53
vbugs1 <42874599+vbugs1@users.noreply.github.com>
2018-09-03 19:45:47 -0300

    Update Projeto_NodeMCU_1.ino
    Atualização Final

--- Projeto_NodeMCU_1.ino
+++ Projeto_NodeMCU_1.ino
@@ -1,5 +1,8 @@
+//Projeto_NodeMCU_1
+//Univates_2018_2_Vicente_Mauricio
+//Sistemas_Microprocessados_Avançados_Dispositivos_para automação_residencial
+
 #include <IRremoteESP8266.h>
-#include <IRsend.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -10,35 +13,24 @@
 #include <PubSubClient.h>
 #define ID_MQTT "Projeto_1"
 #define TOPIC_SUBSCRIBE ""
-#define IR_LED 4    //D2
 
 ESP8266WebServer server(80);
 WiFiClient microsavancados;
 PubSubClient client(microsavancados);
-IRsend irsend (IR_LED);
 
 // Define nome da rede e senha a ser utilizado no SETUP CONFIGURAÇÃO
-const char *ssid = "micros_receptor";
+const char *ssid = "micros_emissor";
 const char *password = "12345678";
 
 //Variáveis internas
 float temperatura;
 char mensagem [100];
-int isterese = 4;
 
 String novaRede;
 String novaSenha;
-String SetPointConfig;
-
-//Estado de funcionamento
-int estado = 0;
-String Status = "Ar Desligado";
-
-int SETPOINT = 25;
-
-//EEPROM
 char rede[30];
 char senha[30];
+
 
 //Váriaveis do MQTT
 const char* BROKER_MQTT = "m10.cloudmqtt.com"; // ip/host do broker
@@ -49,8 +41,6 @@ const char *BROKER_PASSWORD = "HWW0wyt9laS-";
 // Rotinas
 void configura ();
 void salvar ();
-void desliga ();
-void liga ();
 void espaco();
 
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -59,11 +49,12 @@ void espaco();
 
 void setup()
 {
+  // IRsend irsend(4)
 
   //Define entradas (D1 - modo oper.(L) conf. (H) && A0 - sensor de temperatura)
   pinMode (D1, INPUT);
+  pinMode (A0, INPUT);
 
-  pinMode (D0, OUTPUT);   //PINO PARA TESTE
 
 
   //Inicia o monitor serial
@@ -110,8 +101,6 @@ void setup()
   //SETUP OPERAÇÃO
 
   else {
-    irsend.begin();
-
     Serial.println("MODO: operação");
     espaco();
 
@@ -125,10 +114,6 @@ void setup()
     Serial.println(senha);
 
     EEPROM.end();
-
-    Serial.println("Set Point pré ajustado em 25ºC");
-    Serial.println("OBS: Set Point pode ser definido no servidor MQTT");
-
     espaco();
 
     //CONECTA A REDE WIFI
@@ -137,9 +122,8 @@ void setup()
 
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
-      Serial.println(".");
+      Serial.print(".");
     }
-    espaco();
 
     Serial.println("");
     Serial.println("WiFi conectado");
@@ -152,12 +136,12 @@ void setup()
     Serial.println(WiFi.localIP());
 
     client.setServer(BROKER_MQTT, BROKER_PORT);
-    client.setCallback(callback);
+
 
     while (!client.connected()) {
-      Serial.println("Conectado ao servidor MQTT...");
+      Serial.println("Conectando ao servidor MQTT .... ");
 
-      if (client.connect("ESP8266Client", BROKER_USER, BROKER_PASSWORD)) {
+      if (client.connect(ID_MQTT, BROKER_USER, BROKER_PASSWORD)) {
         Serial.println("Conectado");
 
       } else {
@@ -168,12 +152,8 @@ void setup()
       }
       espaco();
     }
-
-    client.subscribe("temperatura");
-    client.subscribe("setpoint");
   }
 }
-
 
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
                                               FUNÇÃO LOOP
@@ -192,6 +172,18 @@ void loop() {
 
   else {
     client.loop();
+
+    //Lê valor de temperatura do sensor conectado a porta A0
+    float temperatura = (analogRead(A0) * 330.0) / 1023.0;    //Lê pino A0 e converte mV para ºC
+    sprintf(mensagem, "%f", temperatura);                     //Converte valor de temperatura (float) to char
+
+    //Publica no servidor o valor lido
+    client.publish("temperatura", mensagem);                  //publica na tag 'temperatura' o valor de temperatura
+
+    Serial.print("Temperatura envidada: ");
+    Serial.println(mensagem);
+    espaco();
+
     delay(2000);
   }
 }
@@ -235,7 +227,7 @@ void salvar () {
   Serial.print("Senha: ");
   Serial.println(novaSenha);
   espaco();
-
+  
   //Salva os dados na memória EEPROM
   novaRede.toCharArray (rede, 30);
   novaSenha.toCharArray(senha, 30);
@@ -254,110 +246,8 @@ void salvar () {
 
 
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-                FUNÇÃO QUE RECEBE OS VALORES PUBLICADOS NO SERVIDOR
+                                  FUNÇÃO ESPAÇO
   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-
-void callback(char* topic, byte* payload, unsigned int length) {
-
-  //Converte a informação (ponteiro) para char
-  int i;
-  char value[100];
-  for (i = 0; i < length; i++) {
-    value[i] = (char)payload[i];
-  }
-  value[i] = '\0';
-
-
-  //Verifica se valor recebido é de setpoint
-  if (strcmp(topic, "setpoint") == 0 ) {
-
-    SETPOINT = int(atof(value));                                    //converte char para float e salva valor em 'setpoint'
-
-    //Salva os dados na memória EEPROM
-    SetPointConfig = (value);
-
-    //Apresenta novo valor de set point
-    espaco();
-    Serial.print("Novo SETPOINT: ");
-    Serial.println(SetPointConfig);
-    espaco();
-  }
-
-  //Verifica se valor recebido é de temperatura
-  if (strcmp(topic, "temperatura") == 0 ) {
-    float temp = atof(value);
-
-    //Apresenta valor de temperatura recebido
-    Serial.print("SetPoint: ");
-    Serial.println(SETPOINT);
-    Serial.println("");
-    Serial.print("Valor de temperatura recebido: ");
-    Serial.println(temp);
-
-    //Verifica se temperatura é maior que setpoint
-
-    if (temp > (SETPOINT + (isterese / 2)) && estado == 0) {
-      liga ();
-      estado = 1;
-      Status = "Ar ligado";
-    }
-
-    //Verifica se temperatura é maior que setpoint
-    if (temp < (SETPOINT - (isterese / 2)) && estado == 1) {
-      desliga();
-      estado = 0;
-      Status = "Ar desligado";
-    }
-
-    //Exibe Status de funcionamento do equipamento (EM FUNÇÃO DO COMANDO LIGA/DESLIGA)
-    Serial.print ("Status: ");
-    Serial.println(Status);
-    espaco();
-  }
-}
-
-
-/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-                                  FUNÇÃO LIGA
-  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-
-void liga() {
-  digitalWrite (D0, LOW);
-
-  int khz = 38;
-
-  uint16_t sinalLiga[] = {4600, 4250, 750, 1450, 750, 400, 700, 1450, 700, 1500, 700, 400, 700, 450, 700, 1500, 700, 450, 700, 450, 700, 1550, 700, 450, 700, 450, 700, 1500, 700, 1550, 700, 450, 700, 1550, 700,
-                          1500, 700, 450, 650, 450, 650, 1550, 650, 1550, 650, 1500, 650, 1550, 650, 1500, 650, 500, 650, 1550, 650, 1550, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 1600, 650, 1550, 650, 500,
-                          650, 500, 650, 500, 650, 500, 650, 1600, 650, 1550, 650, 500, 650, 500, 650, 1600, 650, 1550, 650, 1600, 650, 1550, 650, 5150, 4450, 4350, 650, 1550, 650, 500, 650, 1550, 650, 1600, 650, 500, 650, 500, 650, 1550, 650,
-                          500, 650, 500, 650, 1600, 650, 500, 650, 500, 650, 1550, 650, 1600, 650, 500, 650, 1600, 650, 1600, 650, 500, 650, 500, 650, 1550, 650, 1600, 650, 1550, 650, 1600, 650, 1550, 650, 500, 650, 1600, 650, 1550, 650, 500,
-                          650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 1600, 650, 1550, 650, 500, 650, 500, 650, 500, 600, 500, 600, 1600, 650, 1550, 650, 500, 600, 500, 600, 1600, 650, 1550, 650, 1600, 650, 1600, 650
-                         };
-
-  irsend.sendRaw(sinalLiga, (sizeof(sinalLiga) / sizeof(sinalLiga[0])), khz);  // Send a raw data capture at 38kHz.
-}
-
-
-/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-                                  FUNÇÃO DESLIGA
-  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-
-void desliga () {
-  espaco();
-
-  digitalWrite (D0, HIGH);
-
-  int khz = 38;
-
-  uint16_t sinalDesliga[] = {4600, 4250, 750, 1450, 750, 400, 700, 1450, 700, 1500, 700, 400, 700, 450, 700, 1500, 700, 450, 700, 450, 700, 1550, 700, 450, 700, 450, 700, 1500, 700, 1550, 700, 450, 700, 1550, 700,
-                             450, 700, 1550, 700, 1500, 700, 1550, 650, 1550, 650, 450, 650, 1550, 650, 1500, 650, 1550, 650, 450, 650, 500, 650, 450, 650, 500, 650, 1550, 650, 500, 650, 500, 650, 1600, 650, 1550, 650, 1600, 650, 500, 650, 500,
-                             650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 1550, 650, 1600, 650, 1550, 650, 1600, 650, 1550, 650, 5150, 4450, 4350, 650, 1550, 650, 500, 650, 1550, 650, 1600, 650, 500, 650, 500, 650, 1550, 650,
-                             500, 650, 500, 650, 1600, 650, 500, 650, 500, 650, 1550, 650, 1600, 650, 500, 650, 1600, 650, 500, 650, 1550, 650, 1600, 650, 1550, 650, 1600, 650, 500, 650, 1600, 650, 1550, 650, 1600, 650, 500, 650, 500, 650, 500,
-                             650, 500, 650, 1550, 650, 500, 650, 500, 650, 1600, 650, 1600, 650, 1550, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 600, 1550, 650, 1600, 650, 1550, 650, 1600, 650, 1600, 650
-                            };  // SAMSUNG B24D7B84
-
-  irsend.sendRaw(sinalDesliga, (sizeof(sinalDesliga) / sizeof(sinalDesliga[0])), khz);  // Send a raw data capture at 38kHz.
-}
-
 
 void espaco() {
   Serial.println("--------------------------------------------");
