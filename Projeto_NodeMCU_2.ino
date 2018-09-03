9b63450dd3f0d5e95b53e21e5fb075e39656a9b0
Mauriciolorenzon <42899022+Mauriciolorenzon@users.noreply.github.com>
2018-09-02 23:35:25 -0300

    Update Projeto_NodeMCU_2.ino
    Código editado final leitor de temperatura

--- Projeto_NodeMCU_2.ino
+++ Projeto_NodeMCU_2.ino
@@ -1,6 +1,3 @@
-//Projeto_NodeMCU_2
-//Univates_2018_2_Vicente_Mauricio
-//Sistemas_Microprocessados_Avançados_Dispositivos_para automação_residencial
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -9,91 +6,189 @@
 #include <EEPROM.h>
 #define MEM_ALOC_SIZE 64
 #include <PubSubClient.h>
-//#include <IRsend.h>
+#define ID_MQTT "Projeto_1"
+#define TOPIC_SUBSCRIBE ""
 
 ESP8266WebServer server(80);
+WiFiClient microsavancados;
+PubSubClient client(microsavancados);
 
+// Define nome da rede e senha a ser utilizado no SETUP CONFIGURAÇÃO
+const char *ssid = "micros_emissor";
+const char *password = "12345678";
+
+//Variáveis internas
 float temperatura;
 char mensagem [100];
-char setpoint = 27;
-char novaRede;
-char novaSenha;
 
+String novaRede;
+String novaSenha;
+char rede[30];
+char senha[30];
+
+
+//Váriaveis do MQTT
 const char* BROKER_MQTT = "m10.cloudmqtt.com"; // ip/host do broker
 int BROKER_PORT = 15557; // porta do broker
 const char *BROKER_USER = "fvhdnjss";
 const char *BROKER_PASSWORD = "HWW0wyt9laS-";
 
-// prototypes
-void initPins();
-void initSerial();
-void initWiFi();
-void initMQTT();
-void setup_configuracao();
-void setup_operacao();
-void liga();
-void desliga();
+// Rotinas
+void configura ();
+void salvar ();
+void espaco();
 
-#define ID_MQTT "Projeto_1"
-#define TOPIC_SUBSCRIBE ""
-
-WiFiClient microsavancados;
-PubSubClient client(microsavancados);
-
-
-/*---------------------------------------------------------------------------*/
-/*SETUP*/
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                              SETUP GERAL
+  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
 void setup()
 {
   // IRsend irsend(4)
-  pinMode (D1, INPUT);
-  pinMode (D2, OUTPUT);
 
-  delay(1000);
+  //Define entradas (D1 - modo oper.(L) conf. (H) && A0 - sensor de temperatura)
+  pinMode (D1, INPUT);
+  pinMode (A0, INPUT);
+
+
+
+  //Inicia o monitor serial
   Serial.begin(115200);
   Serial.println();
+  delay(1000);
 
+  espaco();
+  Serial.println("Sistemas Microprocessados Avançados");
+  Serial.println("Trabalho nota 1");
+  Serial.println("Mauricio Lorenzon && Vicente Bugs");
+  espaco();
+
+
+  //SETUP CONFIGURAÇÃO
 
   if (digitalRead(D1) == 1) {
-    //CRIA REDE
-    Serial.println("Configuração");
-    setup_configuracao ();
-  } else {
-    Serial.println("Operação");
-    setup_operacao();
+
+    //Cria rede WiFi
+    Serial.println("MODO: Configuração");
+    espaco();
+
+    //Cria rede com parametros pré definidos
+    Serial.println("Configurando ponto de acesso...");
+    WiFi.softAP(ssid, password);
+    espaco();
+
+
+    //Apresenta endereço de IP
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("Endereço de IP: ");
+    Serial.println(myIP);
+
+    //Se acessar tal IP - direciona para a rotina "CONFIGURA"
+    server.on("/", configura);
+    server.on("/salvar", salvar);
+
+    server.begin();
+    Serial.println("Servidor iniciado");
+    espaco();
+  }
+
+
+  //SETUP OPERAÇÃO
+
+  else {
+    Serial.println("MODO: operação");
+    espaco();
+
+    EEPROM.begin(MEM_ALOC_SIZE);
+    EEPROM.get(0, rede);
+    Serial.print("Rede Wifi cadastrada: ");
+    Serial.println(rede);
+
+    EEPROM.get(30, senha);
+    Serial.print("Senha de rede WiFi cadastrada: ");
+    Serial.println(senha);
+
+    EEPROM.end();
+    espaco();
+
+    //CONECTA A REDE WIFI
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(rede, senha);
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi conectado");
+
+    // Iniciado o servidor
+    server.begin();
+    Serial.println("Iniciando o servidor");
+
+    // Apresenta o endereço de IP
+    Serial.println(WiFi.localIP());
+
+    client.setServer(BROKER_MQTT, BROKER_PORT);
+
+
+    while (!client.connected()) {
+      Serial.println("Conectando ao servidor MQTT .... ");
+
+      if (client.connect("ESP8266Client", BROKER_USER, BROKER_PASSWORD)) {
+        Serial.println("Conectado");
+
+      } else {
+        Serial.print("Falha na conexão ");
+        Serial.print(client.state());
+        delay(2000);
+
+      }
+      espaco();
+    }
   }
 }
 
-/*---------------------------------------------------------------------------*/
-/*CRIA REDE*/
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                              FUNÇÃO LOOP
+  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
-void setup_configuracao () {
+void loop() {
 
-  const char *ssid = "micros_1_B";
-  const char *password = "12345678";
+  //Loop configuração
 
-  Serial.print("Configuring access point...");
-  /* You can remove the password parameter if you want the AP to be open. */
-  WiFi.softAP(ssid, password);
+  if (digitalRead(D1) == 1) {
+    server.handleClient();
+  }
 
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
-  server.on("/", configura);
-  server.begin();
-  Serial.println("HTTP server started");
+
+  //Loop operação
+
+  else {
+    client.loop();
+
+    //Lê valor de temperatura do sensor conectado a porta A0
+    float temperatura = (analogRead(A0) * 330.0) / 1023.0;    //Lê pino A0 e converte mV para ºC
+    sprintf(mensagem, "%f", temperatura);                     //Converte valor de temperatura (float) to char
+
+    //Publica no servidor o valor lido
+    client.publish("temperatura", mensagem);                  //publica na tag 'temperatura' o valor de temperatura
+
+    Serial.print("Temperatura envidada: ");
+    Serial.println(mensagem);
+    espaco();
+
+    delay(2000);
+  }
 }
 
-void loop_configuracao() {
-  server.handleClient();
-}
 
-/*---------------------------------------------------------------------------*/
-/*LÊ DADOS DIGITADOS*/
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                  EM MODO CONFIGURAÇÃO - LÊ OS DADOS DIGITADOS (REDE, SENHA, SETPOINT)
+  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
 void configura () {
-  //MODO CONFIGURAÇÃO - PARA ENTRADA DE NOME DA REDE, SENHA E SET POINT DE OPERAÇÃO
   server.send(200, "text/html",
               "<html>\n"
               "<body>\n"
@@ -102,9 +197,7 @@ void configura () {
               "    <input type=\"text\" name=\"NomeRede\" value=\"Informe a Rede\">\n"
               "    <br> Senha name: <br>\n"
               "    <input type=\"text\" name=\"SenhaRede\" value=\"Informe a senha\">\n"
-              "    <br> Setpoint name: <br>\n"
-              "    <input type=\"text\" name=\"SetPoint\" value=\"SetPoint de temperatura\">\n"
-              "    <br><br>\n"
+              "    <br>\n"
               "    <input type=\"submit\" value=\"Submit\">\n"
               "</form>\n"
               "<p>If you click the \"Submit\" button, the form - data will be sent to a page called \"/salvar\".</p>\n"
@@ -112,144 +205,45 @@ void configura () {
               "</html>"
              );
 }
-/*---------------------------------------------------------------------------*/
-/*SALVA DADOS DE ENTRADA*/
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                   SALVA OS DADOS INFORMADOS NA MEMÓRIA
+  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
 void salvar () {
-  //SALVA NOME DA REDE E SENHA
 
+  novaRede = server.arg("NomeRede");
+  novaSenha = server.arg("SenhaRede");
+
+  //Apresenta os dados informados
+  Serial.print("Nome da rede: ");
   Serial.println(novaRede);
+  Serial.print("Senha: ");
   Serial.println(novaSenha);
-  Serial.println(setpoint);
 
-  server.send(200, "text/html", "<p>ok, senha salva</p>");
+  //Salva os dados na memória EEPROM
+  novaRede.toCharArray (rede, 30);
+  novaSenha.toCharArray(senha, 30);
 
   EEPROM.begin(MEM_ALOC_SIZE);
-  EEPROM.write(0, novaRede);
-  EEPROM.write(1, novaSenha);
-  EEPROM.write(2, setpoint);
+  EEPROM.put(0, rede);
+  EEPROM.put(30, senha);
   EEPROM.end();
+
+  EEPROM.begin(MEM_ALOC_SIZE);
+
+  EEPROM.end();
+
+  server.send(200, "text/html", "<p>Dados salvos com sucesso</p>");
 }
 
-/*---------------------------------------------------------------------------*/
-/*SETUP OPERAÇÃO*/
 
-void setup_operacao() {
-  //CONECTA A REDE WIFI
-  WiFi.mode(WIFI_STA);
-  WiFi.begin("micros_2", "12345678");        // pré-configuração rede wifi para conexão servidor
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                  FUNÇÃO ESPAÇO
+  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+void espaco() {
+  Serial.println("--------------------------------------------");
   Serial.println("");
-  Serial.println("WiFi connected");
-
-  // Start the server
-  server.begin();
-  Serial.println("Server started");
-
-  // Print the IP address
-  Serial.println(WiFi.localIP());
-
-  client.setServer(BROKER_MQTT, BROKER_PORT);
-  client.setCallback(callback);
-
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
-
-    if (client.connect("ESP8266Client", BROKER_USER, BROKER_PASSWORD)) {
-
-      Serial.println("connected");
-
-    } else {
-
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
-
-    }
-  }
-}
-
-
-void callback(char* topic, byte* payload, unsigned int length) {
-
-  Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
-  int temp_ambiente = (int)topic;
-  int set = (int)setpoint;
-
-  if (temp_ambiente > setpoint) {
-    liga ();
-  }
-
-  if (temp_ambiente < setpoint)
-  {
-    desliga();
-  }
-
-  Serial.print("Message:");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-
-  Serial.println();
-  Serial.println("-----------------------");
-
-}
-
-/*---------------------------------------------------------------------------*/
-/*LOOP OPERAÇÃO*/
-
-void loop_operacao() {
-
-
-  client.loop();
-
-
-  delay(2000);
-}
-
-/*---------------------------------------------------------------------------*/
-/*LOOP*/
-
-void loop() {
-
-  //MQTT.loop();
-
-  if (digitalRead(D1) == 1) {
-    loop_configuracao();
-  } else {
-    loop_operacao();
-  }
-}
-
-void liga() {
-  Serial.print("LIGA");/*
-
-  uint16_t liga[] = {4600, 4250, 750, 1450, 750, 400, 700, 1450, 700, 1500, 700, 400, 700, 450, 700, 1500, 700, 450, 700, 450, 700, 1550, 700, 450, 700, 450, 700, 1500, 700, 1550, 700, 450, 700, 1550, 700,
-                     1500, 700, 450, 650, 450, 650, 1550, 650, 1550, 650, 1500, 650, 1550, 650, 1500, 650, 500, 650, 1550, 650, 1550, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 1600, 650, 1550, 650, 500,
-                     650, 500, 650, 500, 650, 500, 650, 1600, 650, 1550, 650, 500, 650, 500, 650, 1600, 650, 1550, 650, 1600, 650, 1550, 650, 5150, 4450, 4350, 650, 1550, 650, 500, 650, 1550, 650, 1600, 650, 500, 650, 500, 650, 1550, 650,
-                     500, 650, 500, 650, 1600, 650, 500, 650, 500, 650, 1550, 650, 1600, 650, 500, 650, 1600, 650, 1600, 650, 500, 650, 500, 650, 1550, 650, 1600, 650, 1550, 650, 1600, 650, 1550, 650, 500, 650, 1600, 650, 1550, 650, 500,
-                     650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 1600, 650, 1550, 650, 500, 650, 500, 650, 500, 600, 500, 600, 1600, 650, 1550, 650, 500, 600, 500, 600, 1600, 650, 1550, 650, 1600, 650, 1600, 650
-                    };
-
-  irsend.sendRaw(desliga, sizeof(desliga) / sizeof(desliga[0]), 38);  // Send a raw data capture at 38kHz.*/
-}
-
-void desliga () {
-  Serial.print("DESLIGA");
-
-  /*
-    uint16_t desliga[] = {4600, 4250, 750, 1450, 750, 400, 700, 1450, 700, 1500, 700, 400, 700, 450, 700, 1500, 700, 450, 700, 450, 700, 1550, 700, 450, 700, 450, 700, 1500, 700, 1550, 700, 450, 700, 1550, 700,
-                          450, 700, 1550, 700, 1500, 700, 1550, 650, 1550, 650, 450, 650, 1550, 650, 1500, 650, 1550, 650, 450, 650, 500, 650, 450, 650, 500, 650, 1550, 650, 500, 650, 500, 650, 1600, 650, 1550, 650, 1600, 650, 500, 650, 500,
-                          650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 1550, 650, 1600, 650, 1550, 650, 1600, 650, 1550, 650, 5150, 4450, 4350, 650, 1550, 650, 500, 650, 1550, 650, 1600, 650, 500, 650, 500, 650, 1550, 650,
-                          500, 650, 500, 650, 1600, 650, 500, 650, 500, 650, 1550, 650, 1600, 650, 500, 650, 1600, 650, 500, 650, 1550, 650, 1600, 650, 1550, 650, 1600, 650, 500, 650, 1600, 650, 1550, 650, 1600, 650, 500, 650, 500, 650, 500,
-                          650, 500, 650, 1550, 650, 500, 650, 500, 650, 1600, 650, 1600, 650, 1550, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 650, 500, 600, 1550, 650, 1600, 650, 1550, 650, 1600, 650, 1600, 650
-                         };  // SAMSUNG B24D7B84
-
-    irsend.sendRaw(desliga, sizeof(desliga) / sizeof(desliga[0]), 38);  // Send a raw data capture at 38kHz.
-  */
 }
